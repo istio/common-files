istio/common-files

commit dfe763eb1963f7e1205625405693c532a60f6f51

Add docker run script (#172)
* Add docker run script

* Break out environment setup into a common script

This allows running the same set of logic for the script runs and
makefile runs. It also allows non makefile users to source this and
continue to run `go test ./...` or whatever they want, without
duplicated logic.

* Fix lint

* clean up

files/Makefile
@@ -30,100 +30,13 @@ SHELL := /bin/bash
 # figure out all the tools you need in your environment to make that work.
 export BUILD_WITH_CONTAINER ?= 0
 
-# Version of image used within build container
-IMAGE_VERSION ?= master-2020-03-05T18-27-04
-
-LOCAL_ARCH := $(shell uname -m)
-ifeq ($(LOCAL_ARCH),x86_64)
-    TARGET_ARCH ?= amd64
-else ifeq ($(shell echo $(LOCAL_ARCH) | head -c 5),armv8)
-    TARGET_ARCH ?= arm64
-else ifeq ($(LOCAL_ARCH),aarch64)
-    TARGET_ARCH ?= arm64
-else ifeq ($(shell echo $(LOCAL_ARCH) | head -c 4),armv)
-    TARGET_ARCH ?= arm
-else
-    $(error This system's architecture $(LOCAL_ARCH) isn't supported)
-endif
-
-LOCAL_OS := $(shell uname)
-ifeq ($(LOCAL_OS),Linux)
-    TARGET_OS ?= linux
-    READLINK_FLAGS="-f"
-else ifeq ($(LOCAL_OS),Darwin)
-    TARGET_OS ?= darwin
-    READLINK_FLAGS=""
-else
-    $(error This system's OS $(LOCAL_OS) isn't supported)
-endif
-
-export TARGET_OUT ?= $(shell pwd)/out/$(TARGET_OS)_$(TARGET_ARCH)
-export TARGET_OUT_LINUX ?= $(shell pwd)/out/linux_amd64
+# Set up environment variables. Using a common script allows the script and makefile to re-use logic
+$(shell mkdir -p out)
+$(shell $(PWD)/common/scripts/setup_env.sh)
 
 ifeq ($(BUILD_WITH_CONTAINER),1)
-export TARGET_OUT = /work/out/$(TARGET_OS)_$(TARGET_ARCH)
-export TARGET_OUT_LINUX = /work/out/linux_amd64
-CONTAINER_CLI ?= docker
-DOCKER_SOCKET_MOUNT ?= -v /var/run/docker.sock:/var/run/docker.sock
-IMG ?= gcr.io/istio-testing/build-tools:$(IMAGE_VERSION)
-UID = $(shell id -u)
-GID = `grep '^docker:' /etc/group | cut -f3 -d:`
-PWD = $(shell pwd)
 
-$(info If you suffer an unexpected failure, please reference: https://github.com/istio/istio/wiki/Troubleshooting-Development-Environment)
-$(info Building with the build container: $(IMG).)
-
-# Determine the timezone across various platforms to pass into the
-# docker run operation. This operation assumes zoneinfo is within
-# the path of the file.
-TIMEZONE=`readlink $(READLINK_FLAGS) /etc/localtime | sed -e 's/^.*zoneinfo\///'`
-
-# Determine the docker.push credential bind mounts.
-# Docker and GCR are supported credentials. At this time docker.push may
-# not work well on Docker-For-Mac. This will be handled in a follow-up PR.
-CONDITIONAL_HOST_MOUNTS:=
-
-ifneq (,$(wildcard $(HOME)/.docker))
-$(info Using docker credential directory $(HOME)/.docker.)
-CONDITIONAL_HOST_MOUNTS+=--mount type=bind,source="$(HOME)/.docker",destination="/config/.docker",readonly
-endif
-
-ifneq (,$(wildcard $(HOME)/.config/gcloud))
-$(info Using gcr credential directory $(HOME)/.config/gcloud.)
-CONDITIONAL_HOST_MOUNTS+=--mount type=bind,source="$(HOME)/.config/gcloud",destination="/config/.config/gcloud",readonly
-endif
-
-ifneq (,$(wildcard $(HOME)/.kube))
-$(info Using local Kubernetes configuration $(HOME)/.kube)
-CONDITIONAL_HOST_MOUNTS+=--mount type=bind,source="$(HOME)/.kube",destination="/home/.kube"
-endif
-
-ENV_VARS:=
-ifdef HUB
-ENV_VARS+=-e HUB="$(HUB)"
-endif
-ifdef TAG
-ENV_VARS+=-e TAG="$(TAG)"
-endif
-
-RUN = $(CONTAINER_CLI) run -t -i --sig-proxy=true -u $(UID):$(GID) --rm \
-	-e IN_BUILD_CONTAINER="$(BUILD_WITH_CONTAINER)" \
-	-e TZ="$(TIMEZONE)" \
-	-e TARGET_ARCH="$(TARGET_ARCH)" \
-	-e TARGET_OS="$(TARGET_OS)" \
-	-e TARGET_OUT="$(TARGET_OUT)" \
-	-e TARGET_OUT_LINUX="$(TARGET_OUT_LINUX)" \
-	-e USER="${USER}" \
-	-e IMAGE_VERSION="$(IMAGE_VERSION)" \
-	$(ENV_VARS) \
-	-v /etc/passwd:/etc/passwd:ro \
-	$(DOCKER_SOCKET_MOUNT) \
-	$(CONTAINER_OPTIONS) \
-	--mount type=bind,source="$(PWD)",destination="/work" \
-	--mount type=volume,source=go,destination="/go" \
-	--mount type=volume,source=gocache,destination="/gocache" \
-	$(CONDITIONAL_HOST_MOUNTS) \
-	-w /work $(IMG)
+RUN = ./common/scripts/run.sh
 
 MAKE = $(RUN) make --no-print-directory -e -f Makefile.core.mk
 
@@ -133,11 +46,13 @@ MAKE = $(RUN) make --no-print-directory -e -f Makefile.core.mk
 default:
 	@$(MAKE)
 
+shell:
+	@$(RUN) /bin/bash
+
 .PHONY: default
 
 else
 
-$(info Building with your local toolchain.)
 export GOBIN ?= $(GOPATH)/bin
 include Makefile.core.mk
 

files/common/scripts/run.sh
@@ -1,0 +1,53 @@
+#!/bin/bash
+
+# WARNING: DO NOT EDIT, THIS FILE IS PROBABLY A COPY
+#
+# The original version of this file is located in the https://github.com/istio/common-files repo.
+# If you're looking at this file in a different repo and want to make a change, please go to the
+# common-files repo, make the change there and check it in. Then come back to this repo and run
+# "make update-common".
+
+# Copyright Istio Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+WD=$(dirname "$0")
+WD=$(cd "$WD"; pwd)
+
+# shellcheck disable=SC1090
+source "${WD}/setup_env.sh"
+
+# Override variables with container specific
+export TARGET_OUT=${CONTAINER_TARGET_OUT}
+export TARGET_OUT_LINUX=${CONTAINER_TARGET_OUT_LINUX}
+
+# $CONTAINER_OPTIONS becomes an empty arg when quoted, so SC2086 is disabled for the
+# following command only
+# shellcheck disable=SC2086
+"${CONTAINER_CLI}" run -it --rm \
+    -u "${UID}:${DOCKER_GID}" \
+    --sig-proxy=true \
+    ${DOCKER_SOCKET_MOUNT:--v /var/run/docker.sock:/var/run/docker.sock} \
+    -v /etc/passwd:/etc/passwd:ro \
+    -v /etc/group:/etc/group:ro \
+    $CONTAINER_OPTIONS \
+    --env-file <(env | grep -v ${ENV_BLOCKLIST}) \
+    -e IN_BUILD_CONTAINER=1 \
+    -e TZ="${TIMEZONE:-$TZ}" \
+    --mount "type=bind,source=${PWD},destination=/work" \
+    --mount "type=volume,source=go,destination=/go" \
+    --mount "type=volume,source=gocache,destination=/gocache" \
+    ${CONDITIONAL_HOST_MOUNTS} \
+    -w /work "${IMG}" "$@"

files/common/scripts/setup_env.sh
@@ -1,0 +1,88 @@
+#!/bin/bash
+
+# WARNING: DO NOT EDIT, THIS FILE IS PROBABLY A COPY
+#
+# The original version of this file is located in the https://github.com/istio/common-files repo.
+# If you're looking at this file in a different repo and want to make a change, please go to the
+# common-files repo, make the change there and check it in. Then come back to this repo and run
+# "make update-common".
+
+# Copyright Istio Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+LOCAL_ARCH=$(uname -m)
+export LOCAL_ARCH
+if [[ ${LOCAL_ARCH} == x86_64 ]]; then
+    export TARGET_ARCH=amd64
+elif [[ ${LOCAL_ARCH} == armv8* ]]; then
+    export TARGET_ARCH=arm64
+elif [[ ${LOCAL_ARCH} == aarch64* ]]; then
+    export TARGET_ARCH=arm64
+elif [[ ${LOCAL_ARCH} == armv* ]]; then
+    export TARGET_ARCH=arm
+else
+    echo "This system's architecture, ${LOCAL_ARCH}, isn't supported"
+    exit 1
+fi
+
+LOCAL_OS=$(uname)
+export LOCAL_OS
+if [[ $LOCAL_OS == Linux ]]; then
+    export TARGET_OS=linux
+    readlink_flags="-f"
+elif [[ $LOCAL_OS == Darwin ]]; then
+    export TARGET_OS=darwin
+    readlink_flags=""
+else
+    echo "This system's OS, $LOCAL_OS, isn't supported"
+    exit 1
+fi
+
+# Build image to use
+export IMAGE_VERSION=master-2020-03-05T18-27-04
+
+export UID
+DOCKER_GID=$(grep '^docker:' /etc/group | cut -f3 -d:)
+export DOCKER_GID
+
+TIMEZONE=$(readlink $readlink_flags /etc/localtime | sed -e 's/^.*zoneinfo\///')
+export TIMEZONE
+
+export TARGET_OUT="${TARGET_OUT:-${PWD}/out/${TARGET_OS}_${TARGET_ARCH}}"
+export TARGET_OUT_LINUX="${TARGET_OUT_LINUX:-${PWD}/out/linux_amd64}"
+
+export CONTAINER_TARGET_OUT="${CONTAINER_TARGET_OUT:-/work/out/${TARGET_OS}_${TARGET_ARCH}}"
+export CONTAINER_TARGET_OUT_LINUX="${CONTAINER_TARGET_OUT_LINUX:-/work/out/linux_amd64}"
+
+export IMG="${IMG:-gcr.io/istio-testing/build-tools:${IMAGE_VERSION}}"
+
+export CONTAINER_CLI="${CONTAINER_CLI:-docker}"
+
+export ENV_BLOCKLIST="${ENV_BLOCKLIST:-^_\|PATH\|SHELL\|EDITOR\|TMUX\|USER\|HOME\|PWD\|TERM\|GO\|rvm\|SSH}"
+
+# Set up conditional host mounts for docker and kubernetes config
+export CONDITIONAL_HOST_MOUNTS=${CONDITIONAL_HOST_MOUNTS:-}
+if [[ -d "${HOME}/.docker" ]]; then
+  CONDITIONAL_HOST_MOUNTS+="--mount type=bind,source=${HOME}/.docker,destination=/config/.docker,readonly "
+fi
+if [[ -d "${HOME}/.config/gcloud" ]]; then
+  CONDITIONAL_HOST_MOUNTS+="--mount type=bind,source=${HOME}/.config/gcloud,destination=/config/.config/gcloud,readonly "
+fi
+if [[ -d "${HOME}/.kube" ]]; then
+  CONDITIONAL_HOST_MOUNTS+="--mount type=bind,source=${HOME}/.kube,destination=/home/.kube "
+fi
+
+export REPO_ROOT=/work

commit 598d8e17eedf0bd5c5610e06caec3876be4f3c5d

Fix build when using non container build
I broke this in #172 because I forgot about the BUILD_WITH_CONTAINER=0
case... This should get things moving again. I have tested this in
https://github.com/istio/istio/pull/21951

files/Makefile
@@ -30,11 +30,9 @@ SHELL := /bin/bash
 # figure out all the tools you need in your environment to make that work.
 export BUILD_WITH_CONTAINER ?= 0
 
-# Set up environment variables. Using a common script allows the script and makefile to re-use logic
-$(shell mkdir -p out)
-$(shell $(PWD)/common/scripts/setup_env.sh)
-
 ifeq ($(BUILD_WITH_CONTAINER),1)
+
+$(shell $(PWD)/common/scripts/setup_env.sh)
 
 RUN = ./common/scripts/run.sh
 
@@ -52,6 +50,13 @@ shell:
 .PHONY: default
 
 else
+
+# If we are not in build container, we need a workaround to get environment properly set
+# Write to file, then include
+$(shell mkdir -p out)
+$(shell $(PWD)/common/scripts/setup_env.sh envfile > out/.env)
+include out/.env
+export out/.env
 
 export GOBIN ?= $(GOPATH)/bin
 include Makefile.core.mk

files/common/scripts/run.sh
@@ -32,6 +32,7 @@ source "${WD}/setup_env.sh"
 # Override variables with container specific
 export TARGET_OUT=${CONTAINER_TARGET_OUT}
 export TARGET_OUT_LINUX=${CONTAINER_TARGET_OUT_LINUX}
+export REPO_ROOT=/work
 
 # $CONTAINER_OPTIONS becomes an empty arg when quoted, so SC2086 is disabled for the
 # following command only

files/common/scripts/setup_env.sh
@@ -85,4 +85,13 @@ if [[ -d "${HOME}/.kube" ]]; then
   CONDITIONAL_HOST_MOUNTS+="--mount type=bind,source=${HOME}/.kube,destination=/home/.kube "
 fi
 
-export REPO_ROOT=/work
+# For non container build, we need to write env to file
+if [[ "${1}" == "envfile" ]]; then
+  echo "TARGET_OUT_LINUX=${TARGET_OUT_LINUX}"
+  echo "TARGET_OUT=${TARGET_OUT}"
+  echo "TIMEZONE=${TIMEZONE}"
+  echo "LOCAL_OS=${LOCAL_OS}"
+  echo "TARGET_OS=${TARGET_OS}"
+  echo "LOCAL_ARCH=${LOCAL_ARCH}"
+  echo "TARGET_ARCH=${TARGET_ARCH}"
+fi