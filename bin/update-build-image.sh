964fcfc17b03df700942a6a6443bcb8a1c18c80b
Kmoneal <kenan.oneal@volunteers.acasi.info>
2023-03-30 15:45:49 -0600

    Simplify tools registry env vars

--- bin/update-build-image.sh
+++ bin/update-build-image.sh
@@ -16,7 +16,10 @@
 set -e
 
 ROOT="$(cd -P "$(dirname -- "$0")" && pwd -P)"
-TOOLS_IMAGE_REGISTRY_LIST_URL="${TOOLS_IMAGE_REGISTRY_LIST_URL:-https://gcr.io/v2/istio-testing/build-tools/tags/list}"
+
+TOOLS_REGISTRY_PROVIDER=${TOOLS_REGISTRY_PROVIDER:-gcr.io}
+PROJECT_ID=${PROJECT_ID:-istio-testing}
+TOOLS_REGISTRY_REPO=${TOOLS_REGISTRY_REPO:-build-tools}
 
 # Allow passing in the new IMAGE_VERSION using that as an environment variable
 # or using --image <IMAGE_VERSION>. The parameter has a higher priority.
@@ -38,7 +41,7 @@ if [ -z "$newBuildImage" ] ; then
   fi
 
   # Get the latest build-tools image for the given release
-  newBuildImage=$(curl -sL $TOOLS_IMAGE_REGISTRY_LIST_URL | jq '."manifest"[]["tag"]' | awk '/'$imageRelease'/ && !/latest/' | sort -r | sed  -e 's/^[[:space:]]*"//' -e 's/".*//' | head -n 1)
+  newBuildImage=$(curl -sL "https://${TOOLS_REGISTRY_PROVIDER}/v2/${PROJECT_ID}/${TOOLS_REGISTRY_REPO}/tags/list" | jq '."manifest"[]["tag"]' | awk '/'$imageRelease'/ && !/latest/' | sort -r | sed  -e 's/^[[:space:]]*"//' -e 's/".*//' | head -n 1)
 
 # If no IMAGE_VERSION is specified and one is not found, output an error
   if [ -z "$newBuildImage" ] ; then
